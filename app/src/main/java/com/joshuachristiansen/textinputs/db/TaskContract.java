1606098ebfdcd6a825343843b418df443180adcc
c0mg3k <c0mg3k@outlook.com>
2018-03-02 13:25:30 -0600

    initial todo commit

---
+++ app/src/main/java/com/joshuachristiansen/textinputs/db/TaskContract.java
@@ -0,0 +1,16 @@
+package com.joshuachristiansen.textinputs.db;
+
+/**
+ * Created by jchristiansen on 3/2/2018.
+ */
+import android.provider.BaseColumns;
+public class TaskContract {
+    public static final String DB_NAME = "com.joshuachristiansen.textinputs.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+        public static final String COL_TASK_TITLE = "title";
+    }
+
+}
